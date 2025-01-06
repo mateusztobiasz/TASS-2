b7704cbc3c64bb7df318655f27ee3081bcaafff3
Jakub Urbański <kuba.urban01@gmail.com>
2025-01-06 22:28:34 +0100

    Set Total_cost from total amount without tip

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import pandas as pd
 # taxis['pickup_date'] = pd.to_datetime(taxis['pickup_datetime'], format="%m/%d/%Y %I:%M:%S %p")
 # taxis['dropoff_date'] = pd.to_datetime(taxis['dropoff_datetime'], format="%m/%d/%Y %I:%M:%S %p")
 
-# taxis = taxis.drop(columns=['vendorid', 'Store_and_fwd_flag', 'pickup_datetime', 'dropoff_datetime', 'rate_code', 'Passenger_count', 'Fare_amount', 'Extra', 'MTA_tax', 'Tolls_amount', 'Ehail_fee', 'Improvement_surcharge', 'Payment_type', 'Trip_type'])
+# taxis['Total_cost'] = taxis['Total_amount'] - taxis['Tip_amount']
+
+# taxis = taxis.drop(columns=['vendorid', 'Store_and_fwd_flag', 'pickup_datetime', 'dropoff_datetime', 'rate_code', 'Passenger_count', 'Fare_amount', 'Extra', 'MTA_tax', 'Tolls_amount', 'Tip_amount', 'Total_amount', 'Ehail_fee', 'Improvement_surcharge', 'Payment_type', 'Trip_type'])
 # taxis.to_pickle('./data/taxis.pkl')
 
 # traffic = pd.read_csv('./data/Automated_Traffic_Volume_Counts.csv')
