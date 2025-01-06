51862e27856f1da00274f8a732f5419b889505bb
Jakub Urbański <kuba.urban01@gmail.com>
2025-01-06 21:48:00 +0100

    Extract potentially useful records from traffic and taxis

--- main.py
+++ main.py
@@ -2,24 +2,48 @@ import pandas as pd
 
 ## Preprocessing
 
-taxis = pd.read_csv('./data/2015_Green_Taxi_Trip_Data_20250106.csv')
+# taxis = pd.read_csv('./data/2015_Green_Taxi_Trip_Data_20250106.csv')
 
-taxis['pickup_date'] = pd.to_datetime(taxis['pickup_datetime'], format="%m/%d/%Y %I:%M:%S %p")
-taxis['dropoff_date'] = pd.to_datetime(taxis['dropoff_datetime'], format="%m/%d/%Y %I:%M:%S %p")
+# taxis['pickup_date'] = pd.to_datetime(taxis['pickup_datetime'], format="%m/%d/%Y %I:%M:%S %p")
+# taxis['dropoff_date'] = pd.to_datetime(taxis['dropoff_datetime'], format="%m/%d/%Y %I:%M:%S %p")
 
-taxis = taxis.drop(columns=['vendorid', 'Store_and_fwd_flag', 'pickup_datetime', 'dropoff_datetime', 'rate_code', 'Passenger_count', 'Fare_amount', 'Extra', 'MTA_tax', 'Tolls_amount', 'Ehail_fee', 'Improvement_surcharge', 'Payment_type', 'Trip_type'])
-taxis.to_pickle('./data/taxis.pkl')
+# taxis = taxis.drop(columns=['vendorid', 'Store_and_fwd_flag', 'pickup_datetime', 'dropoff_datetime', 'rate_code', 'Passenger_count', 'Fare_amount', 'Extra', 'MTA_tax', 'Tolls_amount', 'Ehail_fee', 'Improvement_surcharge', 'Payment_type', 'Trip_type'])
+# taxis.to_pickle('./data/taxis.pkl')
 
-traffic = pd.read_csv('./data/Automated_Traffic_Volume_Counts.csv')
-traffic = traffic.drop(columns=['SegmentID', 'Vol', 'Boro','RequestID'])
+# traffic = pd.read_csv('./data/Automated_Traffic_Volume_Counts.csv')
+# traffic = traffic.drop(columns=['SegmentID', 'Vol', 'Boro','RequestID'])
+# traffic = traffic[traffic['Yr'] == 2015]
 
-df = pd.DataFrame({'year': traffic['Yr'],
-                   'month': traffic['M'],
-                   'day': traffic['D'],
-                   'hour': traffic['HH'],
-                   'minute': traffic['MM']})
+# df = pd.DataFrame({'year': traffic['Yr'],
+#                    'month': traffic['M'],
+#                    'day': traffic['D'],
+#                    'hour': traffic['HH'],
+#                    'minute': traffic['MM']})
 
-traffic['date'] = pd.to_datetime(df)
+# traffic['date'] = pd.to_datetime(df)
 
-traffic.to_pickle('./data/traffic.pkl')
+# traffic.to_pickle('./data/traffic.pkl')
 
+## Extracting data
+
+traffic = pd.read_pickle('./data/traffic.pkl')
+
+traffic_hours_count = traffic.groupby(['Yr', 'M', 'D', 'HH', 'MM'])
+counts = traffic_hours_count.count()
+chosen_traffics = counts[counts['street'] > 100]
+
+print(chosen_traffics)
+chosen_traffics.to_pickle('./data/chosen_traffics.pkl')
+
+taxis = pd.read_pickle('./data/taxis.pkl')
+
+n_taxis = 1000000
+bias = pd.Timedelta(minutes=31)
+taxis_temp = taxis['pickup_date'][0:n_taxis]
+indices = pd.Series([False]*len(taxis_temp))
+for g in pd.to_datetime([f"{t[0]} {t[1]} {t[2]} {t[3]} {t[4]}" for t in chosen_traffics.index], format="%Y %m %d %H %M"):
+    indices = indices | ((g - taxis_temp).abs() < bias)
+
+chosen_taxis = taxis[0:n_taxis][indices]
+chosen_taxis.to_pickle('./data/chosen_taxis.pkl')
+print(chosen_taxis)
