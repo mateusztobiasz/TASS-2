227e3c7fceac2a73ad0f951c6a142a20dfed3552
Jakub Urbański <kuba.urban01@gmail.com>
2025-01-10 15:49:26 +0100

    Request API for multiple paths

--- src/paths_extractor.py
+++ src/paths_extractor.py
@@ -1,9 +1,11 @@
 from typing import Iterator, Tuple
 
 import pandas as pd
+import pickle as pkl
+import time
 
 from src.api_requester import send_request
-from src.utils import TAXIS_CHOSEN_PICKLE
+from src.utils import TAXIS_CHOSEN_PICKLE, TAXIS_STEPS_PICKLE
 
 
 def get_taxis_coordinates() -> Iterator:
@@ -18,14 +20,47 @@ def get_taxis_coordinates() -> Iterator:
     return zip(pickup_coordinates, dropoffs_coordinates)
 
 
-def extract_taxi_path(taxi_coordinate: Tuple[Tuple[float]]) -> list:
+def extract_taxi_path(taxi_coordinate: Tuple[Tuple[float]], index: int) -> list:
     response = send_request(taxi_coordinate, steps="true", tidy="true", waypoints="0;1")
     json_response = response.json()
+
+    if response.status_code != 200:
+        print(f'HTTP ERROR: Request nr {index} for taxi route coordinates: {taxi_coordinate} has ended with error code {response.status_code}')
+
+        if "message" in json_response:
+            print(f'\tmessage: {json_response["message"]}')
+        
+        return list()
+
+    if "matchings" not in json_response:
+        print(f'ERROR: Request nr {index} for taxi route coordinates: {taxi_coordinate} has returned no matchings')
+
+        if "message" in json_response:
+            print(f'\tmessage: {json_response["message"]}')
+        
+        return list()
+
+    if len(json_response["matchings"]) == 0 or "legs" not in json_response["matchings"][0] or len(json_response["matchings"][0]["legs"]) == 0:
+        return list()
 
     return json_response["matchings"][0]["legs"][0]["steps"]
 
 
 if __name__ == "__main__":
     taxis_coordinates = get_taxis_coordinates()
-    steps = extract_taxi_path(next(taxis_coordinates))
-    print(steps)
+
+    i = 0
+    taxis_steps = []
+    for coordinates in taxis_coordinates:
+        taxis_steps.append(extract_taxi_path(coordinates, i))
+        i = i + 1
+        if i == 300: # API is limited to 300 request per minute
+            i = 0
+            print('waiting...')
+            time.sleep(61)
+
+    with open(TAXIS_STEPS_PICKLE, 'wb') as f:
+        pkl.dump(taxis_steps, f)
+
+    # with open(TAXIS_STEPS_PICKLE, 'rb') as f:
+    #     mynewlist = pkl.load(f)
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ N_TAXIS = 1000000
 TAXIS_CSV = f"{DATA_DIR}/2015_Green_Taxi_Trip_Data_20250106.csv"
 TAXIS_PICKLE = f"{DATA_DIR}/taxis.pkl"
 TAXIS_CHOSEN_PICKLE = f"{DATA_DIR}/chosen_taxis.pkl"
+TAXIS_STEPS_PICKLE = f"{DATA_DIR}/steps_taxis.pkl"
 TRAFFICS_CHOSEN_PICKLE = f"{DATA_DIR}/chosen_traffics.pkl"
 TRAFFIC_COUNT = 100
 TRAFFICS_CSV = f"{DATA_DIR}/Automated_Traffic_Volume_Counts.csv"
